41d40b3384659fde306ef447896f1ba340ffb7a4
shrijth-shetty <shrijith.shetty@codecraft.co.in>
2026-02-12 08:39:24 +0000

    added documentation for square function
     added documentaion for multiplication
    
    added documentation

--- add.ts
+++ add.ts
@@ -6,14 +6,31 @@ function carSpeed(distance: number, velocity: number): number {
     return distance * velocity;
 }
 
+/**
+ * 
+ * @param a varibale
+ * @param b variable
+ * @returns 
+ */
 function subtract(a: number, b: number): number {
     return a - b;
 }
 
+/**
+ * 
+ * @param a first vairable
+ * @param b second variable
+ * @returns 
+ */
+
 function mult(a: number, b: number): number {
     return a * b;
 }
-
+/**
+ * 
+ * @param a variable
+ * @returns 
+ */
 function square(a:number):number{
     return a*a;
 }
