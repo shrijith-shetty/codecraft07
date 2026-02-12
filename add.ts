3174b3c5789400b4d27a92ea01221b288af57df8
tejas <tejas.shetty@codecraft.co.in>
2026-02-12 05:15:54 +0000

    added sub

--- add.ts
+++ add.ts
@@ -5,3 +5,9 @@ function add(a: number, b: number): number {
 function carSpeed(distance: number, velocity: number): number {
     return distance * velocity;
 }
+  return a + b;
+}
+
+function sub(a: number, b: number): number {
+  return a + b;
+}
