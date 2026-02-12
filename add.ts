bb2465ffb9e22d089ff9051d65f09fc1190ed3a7
tejas <tejas.shetty@codecraft.co.in>
2026-02-12 06:56:31 +0000

    hi
    more

--- add.ts
+++ add.ts
@@ -1,13 +1,14 @@
 function add(a: number, b: number): number {
-    return a + b;
+  return a + b;
 }
 
 function carSpeed(distance: number, velocity: number): number {
-    return distance * velocity;
+  return distance * velocity;
 }
+
+function suib(a: number, b: number): number {
   return a + b;
 }
 
-function sub(a: number, b: number): number {
-  return a + b;
-}
+//hi my new branch
+//branch name is spaceship
