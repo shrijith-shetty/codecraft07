026a0241cac7dad018136bb11cc07b6451c46f3b
aghe12 <meghashettyj@gmail.com>
2026-02-12 06:54:58 +0000

    added a break function

--- add.ts
+++ add.ts
@@ -1,6 +1,9 @@
 function add(a: number, b: number): number {
     return a + b;
 }
+function break(a: number, b: number): number {
+    return a-b/a;
+}
 
 function carSpeed(distance: number, velocity: number): number {
     return distance * velocity;
