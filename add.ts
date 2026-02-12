c66a2b24af30750da83efe242dde5ff2a91bce10
shrijth-shetty <shrijith.shetty@codecraft.co.in>
2026-02-12 05:18:14 +0000

    added new function to calculate the speed

--- add.ts
+++ add.ts
@@ -1,3 +1,7 @@
-function add(a:number, b:number):number{
-    return a+b;
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function carSpeed(distance: number, velocity: number): number {
+    return distance * velocity;
 }
